820ce6a5d4fa424fbcf3d35f766229e9eb0d5e8e
Hao_Kang <haok1402@gmail.com>
2022-02-05 10:40:37 -0500

    :sparkles: FastAPI instead of Flask

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,14 @@
 import os
+import uvicorn
+from fastapi import FastAPI
 
-from flask import Flask
-
-app = Flask(__name__)
+app = FastAPI()
 
 
-@app.route("/")
-def hello_world():
-    name = os.environ.get("NAME", "World")
-    return "Hello {}!".format(name)
+@app.get('/')
+async def index():
+    return "Hello World!"
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
